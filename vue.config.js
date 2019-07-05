ed1b7d1a8427427edcc1515e1bb1284867a846c4
Benjamin Looi <benjaminlooidt@gmail.com>
2019-07-05 14:01:50 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dev: {
+  },
+
+  build: {
+
+
+    productionSourceMap: false,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+  }
+}
