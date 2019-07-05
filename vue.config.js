c9e7885a21618cc256d5b8a91caa807e4d0c4492
Benjamin Looi <benjaminlooidt@gmail.com>
2019-07-05 14:07:26 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-  dev: {
-  },
-
-  build: {
-
-
-    productionSourceMap: false,
-    // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
-
+  css: {
+    sourceMap: false
   }
 }
