c0425a09d0e69eac7640cbbffec97c8c7e39cd8c
Benjamin Looi <benjaminlooidt@gmail.com>
2022-01-06 15:23:09 +0800

    Update firebase.js

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app"
-import "firebase/firestore"
-import "firebase/storage"
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
 
 var firebaseConfig = {
   apiKey: "AIzaSyA9h8Ujc65cpS3od3sR2-nDl28JxO2pBuE",
@@ -9,7 +9,7 @@ var firebaseConfig = {
   projectId: "r-jerome",
   storageBucket: "r-jerome.appspot.com",
   messagingSenderId: "624472786415",
-  appId: "1:624472786415:web:6c25cf92f32764d8"
+  appId: "1:624472786415:web:6c25cf92f32764d8",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
